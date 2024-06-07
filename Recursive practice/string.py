3719630e2674a8777176542b125ce4f1b75cc7a3
wwwwilliwwww <133821375+WilliamCHWong@users.noreply.github.com>
2024-06-07 11:25:25 -0400

    Update string.py

--- Recursive practice/string.py
+++ Recursive practice/string.py
@@ -4,3 +4,13 @@ def reverse_string(s):
     return reverse_string(s[1:]) + s[0]
 
 print(reverse_string("qwertyuiop"))
+
+def is_palindrome(s):
+    if len(s) < 2:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return (True and is_palindrome(s[1:-1]))
+
+print(is_palindrome("mappam"))
+print(is_palindrome("apple"))
